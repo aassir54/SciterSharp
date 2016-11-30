cfe66d21482942a22076bbf95ae6468a2c6c44e6
midiway <ramon@midi.rs>
2016-11-30 18:10:40 -0200

    yeahh

--- OSXPublishNuget/Program.cs
+++ OSXPublishNuget/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+using SciterSharp;
 
 namespace OSXPublishNuget
 {
@@ -7,7 +10,16 @@ namespace OSXPublishNuget
 	{
 		public static void Main(string[] args)
 		{
-			Process.Start("open", ".");
+			Environment.CurrentDirectory = "/Users/diazfabiana/Desktop/Ramon/SciterSharp/SciterSharp";
+			string nuspec = File.ReadAllText("SciterSharpOSX.nuspec");
+			nuspec = Regex.Replace(nuspec,
+						  "<version>.*?</version>",
+						  "<version>" + LibVersion.AssemblyVersion + "</version>",
+			              RegexOptions.None);
+			File.WriteAllText("SciterSharpOSX.nuspec", nuspec);
+
+			Process.Start("nuget", "pack SciterSharpOSX.nuspec").WaitForExit();
+			Process.Start("nuget", "push SciterSharpOSX." + LibVersion.AssemblyVersion + ".nupkg").WaitForExit();
 		}
 	}
 }
