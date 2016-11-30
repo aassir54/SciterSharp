897ff38a0cd5cdd957c2bc7893c7acc54d983414
midiway <ramon@midi.rs>
2016-11-30 17:57:41 -0200

    bump

---
+++ OSXPublishNuget/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace OSXPublishNuget
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Process.Start("open", ".");
+		}
+	}
+}
