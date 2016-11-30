72ff15cfd7f12e3533b2ab400041c6327da5de49
midiway <ramon@misoftware.rs>
2016-11-30 17:56:20 -0200

    LibVersion

---
+++ SciterSharp/LibVersion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SciterSharp
+{
+	static class LibVersion
+	{
+		public const string AssemblyVersion = "1.0.39";
+	}
+}
