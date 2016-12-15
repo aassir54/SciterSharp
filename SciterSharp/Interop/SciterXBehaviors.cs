03d69e83f5d23da4d503049192eda0830bb9ff0c
midiway <ramon@misoftware.rs>
2016-12-14 22:45:51 -0200

    1.0.48

--- SciterSharp/Interop/SciterXBehaviors.cs
+++ SciterSharp/Interop/SciterXBehaviors.cs
@@ -173,10 +173,10 @@ namespace SciterSharp.Interop
 
 		public enum FOCUS_EVENTS : uint
 		{
-			FOCUS_IN = 0,		// container got focus on element inside it, target is an element that got focus
-			FOCUS_OUT = 1,		// container lost focus from any element inside it, target is an element that lost focus
-			FOCUS_LOST = 2,		// target element lost focus
-			FOCUS_GOT = 3,		// target element got focus
+			FOCUS_OUT = 0,      // container got focus on element inside it, target is an element that got focus
+			FOCUS_IN = 1,       // container lost focus from any element inside it, target is an element that lost focus
+			FOCUS_GOT = 2,      // target element got focus
+			FOCUS_LOST = 3,     // target element lost focus
 			FOCUS_REQUEST = 4,	// bubbling event/request, gets sent on child-parent chain to accept/reject focus to be set on the child (target)
 		}
 
