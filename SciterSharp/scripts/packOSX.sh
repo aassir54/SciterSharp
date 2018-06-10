bec447bf06161f016b85cacde8461b5f137e5409
midiway <ramon@misoftware.rs>
2018-06-10 00:14:20 -0300

    OSX

--- SciterSharp/scripts/packOSX.sh
+++ SciterSharp/scripts/packOSX.sh
@@ -4,3 +4,4 @@ echo ######## Packing '../LibConsole' directory to 'ArchiveResource.cs' ########
 cd "$(dirname "$0")"
 chmod +x packfolder
 ./packfolder ../LibConsole ../ArchiveResource.cs -csharp
+sed -i '' 's/SciterAppResource/SciterSharp/' ./../ArchiveResource.cs
